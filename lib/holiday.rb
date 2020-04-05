6fc5ca31a84e43c63d0cf20dc9cc244c6c69483a
Matt Fleming <matthewfleming8@gmail.com>
2020-04-05 00:14:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,11 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+
+holiday_hash.each do |key, value|
+  
+end
+
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
