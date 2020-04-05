87f921d5cf24855c43c82326bdc4682433a6a514
Matt Fleming <matthewfleming8@gmail.com>
2020-04-05 00:26:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holiday, item|
       puts "#{holiday.to_s.split("_").map {|i| i.capitalize.join(" ")}
       
-    end
+    
 
 end
 
